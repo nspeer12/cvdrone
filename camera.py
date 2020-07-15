05ac98c0fe7af26585f3fe79f67498c7cb51e963
nspeer12 <nspeer252@gmail.com>
2020-07-15 09:42:09 -0400

    object detecton working

---
+++ camera.py
@@ -0,0 +1,50 @@
+import cv2
+import glob
+import numpy as np
+import os
+from detection import *
+
+# Specify device
+cap = cv2.VideoCapture(0)
+
+def record_video(filename, mode = 'r'):
+	i = 0
+	while(True):
+		ret, frame = cap.read()
+		cv2.imshow('video', frame)
+		if( mode == 'r'):
+			cv2.imwrite(filename + str(i) + '.jpg', frame)
+			i += 1
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+def stream(mode = None):
+	i = 0
+	filename = 'stream/frame'
+	while(True):
+		ret, frame = cap.read()
+		frame = detect_frame(frame)
+		cv2.imshow('video', frame)
+		if( mode == 'r'):
+			cv2.imwrite(filename + str(i) + '.jpg', frame)
+			i += 1
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
+
+
+
+def write_to_video(dir, videoName):
+	img_arr = []
+	for filename in sorted(glob.glob(dir + '/*.jpg'), key = os.path.getmtime):
+		print(filename)
+		img = cv2.imread(filename)
+		img_arr.append(img)
+
+	height, width, layers = img_arr[0].shape
+	size = (width, height)
+	out = cv2.VideoWriter(videoName + '.mp4',cv2.VideoWriter_fourcc(*'mp4v'), 30, size)
+
+	for i in range(len(img_arr)):
+		out.write(img_arr[i])
+
+	out.release()
--- main.py
+++ main.py
@@ -1,27 +1,9 @@
-from tello import *
-from stats import *
+from detection import *
+from camera import *
 import cv2
-import time
-import pandas as pd
 
-cols = ['time', 'battery', 'height', ' acceleration', 'baro', 'attitude', 'tof']
+def main():
+	stream()
 
-drone = Tello()
-print(drone.tello_ip)
-#drone.streamon()
-
-for i in range(10):
-	log =[drone.get_time(),
-		drone.get_battery(),
-		drone.get_height(),
-		drone.get_acceleration(),
-		drone.get_attitude(),
-		drone.get_tof(),
-		drone.get_baro()]
-
-	print(log)
-
-drone.streamon()
-print('streamon')
-drone._video_thread()
-print('video thread')
+if __name__ == '__main__':
+	main()
